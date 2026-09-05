4448e43b0fade3ad0bb608d3871097c928a86aed
Trier77 <jacob.aa.trier@gmail.com>
2026-09-05 23:37:55 +0200

    tilføjet post route

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-import express, { request } from "express";
+import express, { request, urlencoded } from "express";
 
 const app = express();
 const port = 3000;
+const meassages = [];
 
-app.use(express.static("public")),
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
+
+
 app.get("/", (request, response) =>{
-    response.render("index");
-})
+    response.render("index", {meassages});
+});
+
+app.post("/ask", (request, response) =>{
+    const question = request.body.question;
+    response.render("index", {question})
+});
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
